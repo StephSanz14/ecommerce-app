bc10b632a509e293402ea312c09b786bb60232d8
Stephany <steph_sap@outlook.es>
2025-12-10 13:58:05 -0600

    spvar

--- src/app/pages/user/check-out/check-out.component.ts
+++ src/app/pages/user/check-out/check-out.component.ts
@@ -84,7 +84,7 @@ export class CheckOutComponent implements OnInit{
       products: cart.products.map(p=>({productId:p.product._id, quantity:p.quantity, price:p.product.price})),
       totalPrice: this.total(), 
       status:'pending',
-      ShippingAddress:'689a09119550ee3e8602145d', 
+      shippingAddress:'689a09119550ee3e8602145d', 
       paymentMethod: '6934f0efba03f09601601927',
       shippingCost: 0,
     } as unknown as Order;
