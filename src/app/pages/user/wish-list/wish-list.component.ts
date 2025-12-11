90ae15551d557f37ad22ac2bda26861d538e743d
Stephany <steph_sap@outlook.es>
2025-12-11 13:56:41 -0600

    toastwlts

--- src/app/pages/user/wish-list/wish-list.component.ts
+++ src/app/pages/user/wish-list/wish-list.component.ts
@@ -4,6 +4,9 @@ import { WishList } from '../../../core/types/WishList';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { WishlistService } from '../../../core/services/wishList/wishList.service';
+import { CartService } from '../../../core/services/cart/cart.service';
+import { ToastService } from '../../../core/services/toast/toast.service';
+
 
 @Component({
   selector: 'app-wishlist',
@@ -15,7 +18,7 @@ import { WishlistService } from '../../../core/services/wishList/wishList.servic
 export class WishListComponent implements OnInit {
   wishlist$: Observable<WishList | null> = of(null);
 
-  constructor(private wishlistService: WishlistService) {}
+  constructor(private wishlistService: WishlistService, private cartService: CartService, private toast:ToastService) {}
 
   ngOnInit(): void {
     this.wishlist$ = this.wishlistService.wishlist$;
@@ -27,5 +30,20 @@ export class WishListComponent implements OnInit {
 
   clear() {
     this.wishlistService.clearWishlist().subscribe();
+  }
+
+   addToCart(productId: string) {
+    if (!productId) return;
+
+    this.cartService.addToCart(productId, 1).subscribe({
+      next: () => {
+        this.toast.success('Producto agregado al carrito');
+        this.wishlistService.removeFromWishlist(productId).subscribe();
+      },
+      error: (err) => {
+        console.error('Error al agregar al carrito desde wishlist', err);
+        this.toast.error('No se pudo agregar al carrito');
+      }
+    });
   }
 }
