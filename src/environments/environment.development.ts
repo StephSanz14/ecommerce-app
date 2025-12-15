67098fd7de73ab4b3188dbae137f0456323d0490
Stephany <steph_sap@outlook.es>
2025-12-15 16:33:27 -0600

    enviromentupdate

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,9 +1,10 @@
-/* export const environment = {
+ export const environment = {
+  production: true,
     BACK_URL:'https://proyectofinal-tp1f.onrender.com/api'  //de back
     
 };
- */
-export const environment = {
+ 
+/* export const environment = {
   production: false,
   BACK_URL: 'http://localhost:3000/api',
-};
+}; */
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,9 @@
-export const environment = {
+/* export const environment = {
   production: true,
   BACK_URL: 'https://proyectofinal-tp1f.onrender.com/api',
+}; */
+
+export const environment = {
+  production: false,
+  BACK_URL: 'http://localhost:3000/api'
 };
